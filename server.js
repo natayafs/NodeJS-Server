7fda8d65e279342251fcb9b6d0cc8967b49c96f1
Nata <natayafs@gmail.com>
2020-03-06 17:57:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var http = require("http");
+var url = require("url");
 
 function iniciar(){
 	function onRequest(request, response){
+    var pathname = url.parse(request.url).pathname;
+		console.log("Request a "+ pathname + "recibido");
 		console.log("Request recibido");
-    	response.writeHead(200, {"Content-Type": "text/html"});
-    	response.write("Holis");
-    	response.end();
-      console.log("Request terminado");
-    }).listen(8080);
 
+    response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("Holis");
+		response.end();
+		console.log("Request terminado");
+  }
+    http.createServer(onRequest).listen(8080);
     console.log("Servidor inicial");
 }
 export.iniciar = iniciar;
