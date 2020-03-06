95cf7f2c8ea8e8340c4ae25212cb003182beb59a
Nata <natayafs@gmail.com>
2020-03-06 17:53:34 -0300

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require("http");
 
-http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("holis");
-	response.end();
+function iniciar(){
+	function onRequest(request, response){
+		console.log("Request recibido");
+    	response.writeHead(200, {"Content-Type": "text/html"});
+    	response.write("Holis");
+    	response.end();
+      console.log("Request terminado");
+    }).listen(8080);
 
-}).listen(8080);
+    console.log("Servidor inicial");
+}
+export.iniciar = iniciar;
