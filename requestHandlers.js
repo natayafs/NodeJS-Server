999f47a5d2326ab8ee1b495693f40dc233cbbe8e
Nata <natayafs@gmail.com>
2020-03-08 03:25:33 -0300

    Create requestHandlers.js

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function iniciar () {
+  console.log("Invocando el handler de iniciar");
+}
+
+function subir() {
+  console.log("Invocando el handler de subir");
+}
+
+export.iniciar = iniciar;
+export.subir = subir;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require("http");
 var url = require("url");
 
-function iniciar(route){
+function iniciar(route, handle){
 	function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
 		console.log("Request a "+ pathname + "recibido");
 		console.log("Request recibido");
 
-		route(pathname);
+		route(pathname, handle);
 
     response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("Holis");
